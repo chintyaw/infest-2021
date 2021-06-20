e17e87c23e591ee35aa7801c2903562cb8c0639a
graciellavl <graciellavl@users.noreply.github.com>
2021-06-20 21:12:11 +0700

    fix navbar bug

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,62 +1,63 @@
-import React, { useState } from 'react'
-import { Button } from './Button'
-import { Link } from 'react-router-dom'
-import './Navbar.css'
-import Dropdown from './Dropdown'
+import React, { useState } from "react";
+import { Button } from "./Button";
+import { Link } from "react-router-dom";
+import "./Navbar.css";
+import Dropdown from "./Dropdown";
 
 function Navbar() {
-    const [click, setClick] = useState(false);
-    const [dropdown, setDropdown] = useState(false);
+  const [click, setClick] = useState(false);
+  const [dropdown, setDropdown] = useState(false);
 
-    const handleClick = () => setClick(!click);
-    const closeMobileMenu = () => setClick(false);
+  const handleClick = () => setClick(!click);
+  const closeMobileMenu = () => setClick(false);
 
-    const onMouseEnter = () => {
-        if(window.innerWidth < 960){
-            setDropdown(false);
-        }
-        else {
-            setDropdown(true);
-        }
+  const onMouseEnter = () => {
+    if (window.innerWidth < 960) {
+      setDropdown(false);
+    } else {
+      setDropdown(true);
     }
+  };
 
-    const onMouseLeave = () => {
-        if(window.innerWidth < 960){
-            setDropdown(false);
-        }
-        else {
-            setDropdown(false);
-        }
+  const onMouseLeave = () => {
+    if (window.innerWidth < 960) {
+      setDropdown(false);
+    } else {
+      setDropdown(false);
     }
+  };
 
-    return (
-        <>
-            <nav className='navbar'>
-                <Link to='/' className='navbar-logo'>
-                    INFEST
-                </Link>
-                <div className='menu-icon' onClick={handleClick}>
-                    <i className={click ? 'fas fa-times' : 'fas fa-bars'} />
-                </div>
-                <ul className={click ? 'nav-menu active' : 'nav-menu'}>
-                    <li className='nav-item'>
-                        <Link to='/tentang' className='nav-links' onClick={closeMobileMenu}>
-                            Tentang
-                        </Link>
-                    </li>
+  return (
+    <>
+      <nav className="navbar">
+        <Link to="/" className="navbar-logo">
+          INFEST
+        </Link>
+        <div className="menu-icon" onClick={handleClick}>
+          <i className={click ? "fas fa-times" : "fas fa-bars"} />
+        </div>
+        <ul className={click ? "nav-menu active" : "nav-menu"}>
+          <li className="nav-item">
+            <Link to="/tentang" className="nav-links" onClick={closeMobileMenu}>
+              Tentang
+            </Link>
+          </li>
 
-                    <li className='nav-item' onMouseEnter={onMouseEnter} onMouseLeave={onMouseLeave}>
-
-                        <Link to='/acara' className='nav-links' onClick={closeMobileMenu}>
-                            Acara <i className='fas fa-caret-down'/>
-                        </Link>
-                        {dropdown && <Dropdown/>}
-                    </li>
-
-                </ul>
-            </nav>
-        </>
-    )
+          <li
+            className="nav-item"
+            onMouseEnter={onMouseEnter}
+            onMouseLeave={onMouseLeave}
+          >
+            <Link to="/acara" className="nav-links" onClick={closeMobileMenu}>
+              Acara <i className="fas fa-caret-down" />
+            </Link>
+            {/* <Dropdown /> */}
+            {dropdown && <Dropdown />}
+          </li>
+        </ul>
+      </nav>
+    </>
+  );
 }
 
 export default Navbar;

2f50b311f957978dd1908e7b0a5378e58bfb0164
chintyaw <cwchintya@gmail.com>
2021-06-25 03:22:14 +0700

    fix warning

--- src/components/TimelineCompe.js
+++ src/components/TimelineCompe.js
@@ -5,7 +5,7 @@ const TimelineCompe = () => {
         <div>
             <h1 style={{ fontSize: "36px", textAlign: "center", marginBottom: "62px"}}>Timeline</h1>
             <div style={{ width: "100%", display: "flex", justifyContent: "center"}}>
-                <img style={{ marginBottom: "200px", width: "100%" }} src="/timeline-compe.svg" />
+                <img style={{ marginBottom: "200px", width: "100%" }} src="/timeline-compe.svg" alt="/"/>
             </div>
         </div>
     )

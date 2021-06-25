c67783084227df032c145f3058aea036ce1b27db
chintyaw <cwchintya@gmail.com>
2021-06-26 01:11:47 +0700

    scroll

--- src/App.js
+++ src/App.js
@@ -1,28 +1,31 @@
 import React from 'react';
 import './App.css';
 import Navbar from './components/Navbar';
-import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Webinar from './components/pages/Webinar'
 import Workshop from './components/pages/Workshop'
 import Competition from './components/pages/Competition'
 import Acara from './components/pages/Acara'
 import Footer from './components/Footer'
 import Home from './components/pages/Home'
+import ScrollToTop from './components/ScrollToTop';
 
 function App() {
   return (
-    <div style={{backgroundImage: "url(/background.webp)", backgroundSize: "100%"}}>
-    <Router>
-      <Navbar />
-      <Switch>
-        <Route path='/' exact component={Home} />
-        <Route path='/webinar' exact component={Webinar} />
-        <Route path='/workshop' exact component={Workshop} />
-        <Route path='/competition' exact component={Competition} />
-        <Route path='/acara' exact component={Acara} />
-      </Switch>
-    </Router>
-    <Footer />
+    <div style={{ backgroundImage: "url(/background.webp)", backgroundSize: "100%" }}>
+      <Router>
+        <ScrollToTop>
+          <Navbar />
+          <Switch>
+            <Route path='/' exact component={Home} />
+            <Route path='/webinar' exact component={Webinar} />
+            <Route path='/workshop' exact component={Workshop} />
+            <Route path='/competition' exact component={Competition} />
+            <Route path='/acara' exact component={Acara} />
+          </Switch>
+        </ScrollToTop>
+      </Router>
+      <Footer />
     </div>
   );
 }

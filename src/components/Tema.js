85c1b8cad11054b013cc21674218c1e36f414cd0
chintyaw <cwchintya@gmail.com>
2021-06-25 21:52:15 +0700

    add tema

---
+++ src/components/Tema.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import './Tema.css'
+
+const Tema = () => {
+    return (
+        <div className="TemaContainer">
+            <div className="GambarTema">
+                <img src="/infest-logo.svg" width="80%" margin="auto" />
+            </div>
+            <div className="TulisanTema">
+                <div className="JudulTema">
+                    Rise Up and Invest
+                </div>
+                <div className="TemaDesc">
+                    INFEST 2021 mengusung tema “Rise Up and Invest” yaitu suatu ajakan kepada masyarakat Indonesia untuk bangkit dari kondisi ekonomi yang terpuruk dengan cara berinvestasi
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Tema
--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -2,12 +2,14 @@ import React from 'react'
 import '../../App.css'
 import JudulInfest from '../JudulInfest'
 import Tentang from '../Tentang'
+import Tema from '../Tema'
 
 export default function Home(){
     return (
         <div style={{backgroundImage: "url(/background.svg)", backgroundSize: "100%"}}>
             <JudulInfest />
             <Tentang />
+            <Tema />
         </div>
     )
 }

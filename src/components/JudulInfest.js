45a20a64ce9725bce37576865435ef7ec0e2cd5b
chintyaw <cwchintya@gmail.com>
2021-06-25 20:41:06 +0700

    update judul

--- src/components/JudulInfest.js
+++ src/components/JudulInfest.js
@@ -2,14 +2,12 @@ import React from 'react'
 
 const JudulInfest = () => {
     return (
-        <div>
-            <div style={{ width: "100%", display: "flex", justifyContent: "center" }}>
-                <img style={{ marginBottom: "20px", marginTop: "120px", width: "70%", height: "auto" }} src="/judul-infest.svg" alt="/" />
+        <div style={{ margin: "120px 0" }} className="Judul">
+            <div className="GambarJudul" style={{ width: "100%", display: "flex", justifyContent: "center" }}>
+                <img style={{ marginBottom: "20px", width: "70%", height: "auto" }} src="/judul-infest.svg" alt="/" />
             </div>
-            <div style={{ marginBottom: "120px" }}>
-                <h1 style={{ textAlign: "center", marginLeft: "20px", marginRight: "20px" }} >The Biggest Investment Festival held by KSEP ITB for Indonesia</h1>
-            </div>
-        </div >
+            <h1 style={{ textAlign: "center", marginLeft: "20px", marginRight: "20px" }} >The Biggest Investment Festival held by KSEP ITB for Indonesia</h1>
+        </div>
     )
 }
 

0154b407ff590905f9bdd23d598f67e0f6eca421
chintyaw <cwchintya@gmail.com>
2021-06-25 21:06:30 +0700

    add about

---
+++ src/components/Tentang.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import './Tentang.css'
+
+const Tentang = () => {
+    return (
+        <div className="TentangContainer">
+            <h1 style={{textAlign: "center", marginBottom: "20px"}} >Apa itu INFEST?</h1>
+            <div className="Tentang">
+                Investment Festival (INFEST) adalah acara tahunan yang diselenggarakan oleh Kelompok Studi Ekonomi dan Pasar Modal (KSEP) ITB berupa seminar mengenai investasi dan pasar modal untuk mengajak masyarakat Indonesia berinvestasi.
+            </div>
+        </div>
+    )
+}
+
+export default Tentang
--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import '../../App.css'
 import JudulInfest from '../JudulInfest'
+import Tentang from '../Tentang'
 
 export default function Home(){
     return (
         <div style={{backgroundImage: "url(/background.svg)", backgroundSize: "100%"}}>
             <JudulInfest />
+            <Tentang />
         </div>
     )
 }

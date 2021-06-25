ad119c52b173be7ab0b4fd31bafda026507e0a40
chintyaw <cwchintya@gmail.com>
2021-06-25 20:34:23 +0700

    fix button, add judul home

---
+++ src/components/JudulInfest.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const JudulInfest = () => {
+    return (
+        <div>
+            <div style={{ width: "100%", display: "flex", justifyContent: "center" }}>
+                <img style={{ marginBottom: "20px", marginTop: "120px", width: "70%", height: "auto" }} src="/judul-infest.svg" alt="/" />
+            </div>
+            <div style={{ marginBottom: "120px" }}>
+                <h1 style={{ textAlign: "center", marginLeft: "20px", marginRight: "20px" }} >The Biggest Investment Festival held by KSEP ITB for Indonesia</h1>
+            </div>
+        </div >
+    )
+}
+
+export default JudulInfest
--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,10 +1,11 @@
 import React from 'react'
 import '../../App.css'
+import JudulInfest from '../JudulInfest'
 
 export default function Home(){
     return (
         <div style={{backgroundImage: "url(/background.svg)", backgroundSize: "100%"}}>
-            <h1 className='home'> Home </h1>
+            <JudulInfest />
         </div>
     )
 }

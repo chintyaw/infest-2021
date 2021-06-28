efb27f41374a7d89d58876a6732175755b23016b
chintyaw <cwchintya@gmail.com>
2021-06-28 11:15:40 +0700

    jotform

--- src/components/pages/RegistCompetition.js
+++ src/components/pages/RegistCompetition.js
@@ -1,8 +1,7 @@
 import React from 'react'
 import '../../App.css'
 import JotformEmbed from 'react-jotform-embed';
- 
-
+import './RegistCompetition.css'
 
 const RegistCompetition = () => {
     return (

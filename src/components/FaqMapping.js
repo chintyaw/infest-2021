c6b4a9d9c010b21491c1222960c1c3c98ea70efe
xlonz-afk <christianaugust23@gmail.com>
2021-06-29 00:48:28 +0700

    fix styling

--- src/components/FaqMapping.js
+++ src/components/FaqMapping.js
@@ -4,12 +4,12 @@ import SingleQuestion from './FaqExpand'
 import './FaqStyle.css'
 
 const AAA = () => {
-  const [questions, setQuestions] = useState(data)
+  const [questions] = useState(data)
 
   return (
     <main>
       <div className='container'>
-        <h1 style={{textAlign: 'center', fontSize : '40px', marginBottom :'40px'}}>FAQ</h1>
+        <h1 style={{textAlign: 'center', fontSize : '36px', marginBottom :'60px'}}>Frequently Asked Question</h1>
         <section className='info'>
           {questions.map((question) => (
             <SingleQuestion key={question.id} {...question} />
--- src/components/FaqPertanyaan.js
+++ src/components/FaqPertanyaan.js
@@ -3,42 +3,39 @@ const questions = [
     id: 1,
     title: 'Berapa biaya pendaftaran Trading Competition?',
     info: 
-        <text style={{lineHeight:'2'}}>
+        <text style={{lineHeight:'2', fontSize:'18px'}}>
           Pendaftaran Trading Competition dibagi menjadi dua tahap dengan pembagian sebagai berikut: <br/>
-          {'\u2B24'} <text style={{fontStyle: 'italic'}}>Early bird</text> (2 Juli 2021 - 11 Juli 2021) : Rp 100.000,00/tim <br/>
-          {'\u2B24'} <text style={{fontStyle: 'italic'}}>Normal wave</text> (12 Juli 2021 - 25 Juli 2021) : Rp 150.000,00/tim
+          <span>&#8226;</span> <text style={{fontStyle: 'italic'}}>Early bird</text> (2 Juli 2021 - 11 Juli 2021) : Rp 100.000,00/tim <br/>
+          <span>&#8226;</span> <text style={{fontStyle: 'italic'}}>Normal wave</text> (12 Juli 2021 - 25 Juli 2021) : Rp 150.000,00/tim
         </text>
   },
   {
     id: 2,
     title: 'Bagaimana mendaftar Trading Competition',
-    info: <text style={{lineHeight: '2'}}>{'\u2B24'} Mengisi formulir pendaftaran melalui (...)<br/>
-                {'\u2B24'} Mendapatkan konfirmasi registrasi maksimal H+1 setelah pembayaran <br/>
-                Jam 19.00 <text style={{fontStyle : 'italic'}}>cut off</text> pendaftaran di hari itu    
+    info: <text style={{lineHeight: '2', fontSize:'18px'}}><span>&#8226;</span> Mengisi formulir pendaftaran melalui <a href='/registrasicompetition'><b><i>click here</i></b></a><br/>
+                <span>&#8226;</span> Mendapatkan konfirmasi registrasi maksimal H+1 setelah pembayaran <br/>   
           </text> 
   },
   {
     id: 3,
     title: 'Apakah bisa melakukan refund setelah melakukan pembayaran?',
-    info: 'Pembayaran yang telah dilakukan tidak dapat di-refund dengan alasan apapun kecuali terjadi pembatalan acara dari pihak panitia'
+    info: <text style={{fontSize:'18px'}}>Pembayaran yang telah dilakukan tidak dapat di-refund dengan alasan apapun kecuali terjadi pembatalan acara dari pihak panitia</text>
   },
   {
     id: 4,
     title: 'Apakah seluruh peserta Trading Competition akan mendapatkan sertifikat?',
     info:
-      'Iya, seluruh peserta akan mendapat sertifikat ',
+      <text style={{fontSize:'18px'}}>Iya, seluruh peserta akan mendapat sertifikat</text>
   },
   {
     id: 5,
     title: 'Platform apa yang digunakan untuk Trading Competition?',
-    info:
-      'IDX',
+    info: <text style={{fontSize:'18px'}}>IDX</text>
   },
   {
     id: 6,
     title: 'Kapan dan di mana sertifikat diberikan?',
-    info:
-      'Seluruh sertifikat akan dibagikan melalui e-mail ketua tim masing-masing maksimal',
+    info: <text style={{fontSize:'20px'}}>Seluruh sertifikat akan dibagikan melalui e-mail ketua tim masing-masing maksimal</text>
   },
 ]
 export default questions

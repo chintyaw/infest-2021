326dc3aead4e35969fa70807c2e8cfe7c1e20719
chintyaw <cwchintya@gmail.com>
2021-06-28 19:07:56 +0700

    fix shadow

--- src/components/FaqExpand.js
+++ src/components/FaqExpand.js
@@ -7,7 +7,7 @@ const Question = ({ title, info }) => {
   return (
     <article className={expanded? 'questionOpened':'question'}>
       <header>
-        <h1 onClick={() => setExpanded(!expanded)} className='question-title'>
+        <h1 style={{ textShadow: "none"}} onClick={() => setExpanded(!expanded)} className='question-title'>
           {title}
         </h1>
         <button className={expanded? 'btnOpened':'btn'} onClick={() => setExpanded(!expanded)}>

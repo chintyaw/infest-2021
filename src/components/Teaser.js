6a0ee3ff7a2c629160283a446bf0a4572fa72a61
chintyaw <cwchintya@gmail.com>
2021-07-01 15:03:57 +0700

    fix vid

--- src/components/Teaser.js
+++ src/components/Teaser.js
@@ -2,8 +2,21 @@ import React from 'react'
 
 const Teaser = () => {
     return (
-        <div style={{ display: "flex", justifyContent: "center", paddingTop: "90px", marginBottom: "150px"}}>
-            <iframe width="560" height="315"  src="https://www.youtube.com/embed/N1sMU-rIyMI" title="Teaser INFEST 2021" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+        <div className="video-responsive" style={{ display: "flex", justifyContent: "center", paddingTop: "90px", marginBottom: "180px" }}>
+            <iframe
+                width="640"
+                height="360"
+                src="https://www.youtube.com/embed/N1sMU-rIyMI"
+                title="Teaser INFEST 2021"
+                frameborder="0"
+                allow="accelerometer; 
+            autoplay; 
+            clipboard-write; 
+            encrypted-media; 
+            gyroscope; 
+            picture-in-picture"
+                allowfullscreen>
+            </iframe>
         </div>
     )
 }

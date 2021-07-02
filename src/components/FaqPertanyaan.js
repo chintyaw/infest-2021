a326605705b5d8107b277c238673381d8062a847
chintyaw <cwchintya@gmail.com>
2021-07-02 18:08:27 +0700

    add qna

--- src/components/FaqPertanyaan.js
+++ src/components/FaqPertanyaan.js
@@ -2,19 +2,19 @@ const questions = [
   {
     id: 1,
     title: 'Berapa biaya pendaftaran Trading Competition?',
-    info: 
-        <text>
-          Pendaftaran Trading Competition dibagi menjadi dua tahap dengan pembagian sebagai berikut: <br/>
-          <span>&#8226;</span> <text style={{fontStyle: 'italic'}}>Early bird</text> (2 Juli 2021 - 11 Juli 2021) : Rp 100.000,00/tim <br/>
-          <span>&#8226;</span> <text style={{fontStyle: 'italic'}}>Normal wave</text> (12 Juli 2021 - 25 Juli 2021) : Rp 150.000,00/tim
-        </text>
+    info:
+      <text>
+        Pendaftaran Trading Competition dibagi menjadi dua tahap dengan pembagian sebagai berikut: <br />
+        <span>&#8226;</span> <text style={{ fontStyle: 'italic' }}>Early bird</text> (2 Juli 2021 - 11 Juli 2021) : Rp 100.000,00/tim <br />
+        <span>&#8226;</span> <text style={{ fontStyle: 'italic' }}>Normal wave</text> (12 Juli 2021 - 25 Juli 2021) : Rp 150.000,00/tim
+      </text>
   },
   {
     id: 2,
     title: 'Bagaimana mendaftar Trading Competition',
-    info: <text><span>&#8226;</span> Mengisi formulir pendaftaran melalui <a href='/registrasicompetition'><b><i>link berikut</i></b></a><br/>
-                <span>&#8226;</span> Mendapatkan konfirmasi registrasi maksimal H+1 setelah pembayaran <br/>   
-          </text> 
+    info: <text><span>&#8226;</span> Mengisi formulir pendaftaran melalui <a href='/registrasicompetition'><b><i>link berikut</i></b></a><br />
+      <span>&#8226;</span> Mendapatkan konfirmasi registrasi maksimal H+1 setelah pembayaran <br />
+    </text>
   },
   {
     id: 3,
@@ -27,13 +27,13 @@ const questions = [
     info:
       <text>Ya, seluruh peserta akan mendapat sertifikat</text>
   },
-  // {
-  //   id: 5,
-  //   title: 'Platform apa yang digunakan untuk Trading Competition?',
-  //   info: <text>IDX Trading</text>
-  // },
   {
     id: 5,
+    title: 'Apakah mahasiswa baru angkatan 2021 dapat mendaftar trading competition?',
+    info: <text>Untuk peserta yang merupakan mahasiswa baru angkatan 2021 bisa mendaftar dengan melampirkan bukti diterima pada universitas yg akan menjadi tempat berkuliah dalam waktu dekat ini.<br/><br/>Namun, peserta tetap diminta untuk melampirkan KTM yg dapat disusulkan di tengah2 perlombaan dengan mengirimkannya melalui CP yg tersedia. Apabila peserta tidak dapat mengirimkan KTM/KSM/Surat Keterangan Aktif Kuliah sampai batas di batas waktu yang ditentukan yaitu h-1 babak final, maka peserta akan di diskualifikasi.</text>
+  },
+  {
+    id: 6,
     title: 'Kapan dan di mana sertifikat diberikan?',
     info: <text>Seluruh sertifikat akan dibagikan melalui e-mail ketua tim masing-masing maksimal H+7 setelah acara berakhir</text>
   },

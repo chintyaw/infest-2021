9fbb122f5917e05443cfde7a1ac9a2db5afd1083
Maria Khelli <71018093+khelli07@users.noreply.github.com>
2021-07-17 21:27:22 +0700

    added speaker

--- src/components/pages/Webinar.js
+++ src/components/pages/Webinar.js
@@ -1,10 +1,12 @@
-import React from 'react'
-import '../../App.css'
+import React from "react";
+import "../../App.css";
+import Speaker from "../Speaker";
 
-export default function Webinar(){
-    return (
-        <>
-            <h1 className='webinar'> Coming Soon </h1>
-        </>
-    )
+export default function Webinar() {
+  return (
+    <>
+      <h1 className="webinar"> Coming Soon </h1>
+      <Speaker />
+    </>
+  );
 }

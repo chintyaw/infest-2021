157278cafe8d245d692ab15de88d55f4416713f0
chintyaw <cwchintya@gmail.com>
2021-07-17 22:05:06 +0700

    add about workshop

--- src/components/WorkshopAbout.js
+++ src/components/WorkshopAbout.js
@@ -8,7 +8,7 @@ const WorkshopAbout = () => {
             </h1>
             <div className="AboutContainer">
                 <p className="Paragraph">
-                Workshop merupakan
+                Workshop INFEST 2021 merupakan acara tahunan yang kembali digelar oleh KSEP ITB. Pada tahun ini, tema yang diambil untuk workshop adalah Guide To Make Smart Financial Decisions. Dalam kegiatan workshop ini, para peserta yang hadir berkesempatan langsung untuk mendengarkan pemaparan sekaligus melakukan praktek untuk mengatur keuangan bersama pembicara. Melalui workshop Guide To Make Smart Financial Decisions, masyarakat diharapkan dapat merencanakan keputusan finansialnya dan berkurangnya masyarakat yang terjebak dalam keputusan finansial yang salah.
                 </p>
             </div>
         </div>

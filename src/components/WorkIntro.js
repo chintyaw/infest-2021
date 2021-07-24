df39fc8e21846c472d454c3faa77a4974a9cd388
riyanw13 <86229712+riyanw13@users.noreply.github.com>
2021-07-24 21:51:02 +0700

    Add files via upload

---
+++ src/components/WorkIntro.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import './Compe.css'
+
+const WorkIntro = () => {
+    return (
+        <div style={{ margin: "0", paddingTop: "180px", paddingBottom: "40px"}}>
+            <h1 style={{textAlign: "center",paddingBottom: "40px", width: "80%", margin: "auto" }}>
+                WORKSHOP
+            </h1>
+            <div className="IntroContainer">
+                <div className="Paragraph" style={{ width: "50%" }}>
+                Workshop online berisi materi sekaligus praktek langsung mengenai investasi dan pasar modal oleh ahli dalam bidangnya.
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default WorkIntro

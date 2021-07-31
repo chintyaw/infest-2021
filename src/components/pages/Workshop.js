649d64a1694b6ac021268879f31d4da5ed5a57d5
Vincent541 <80274903+VincentChristian541@users.noreply.github.com>
2021-07-31 23:33:41 +0700

    add biaya dan benefit

--- src/components/pages/Workshop.js
+++ src/components/pages/Workshop.js
@@ -2,12 +2,14 @@ import React from 'react'
 import '../../App.css'
 import WorkshopAbout from '../WorkshopAbout'
 import BottomRegistWork from '../BottomRegistWork';
+import BenefitBiayaWorkshop from '../BenefitBiayaWorkshop';
 
 export default function Workshop(){
     return (
         <>
             <h1 className='workshop'> Coming Soon </h1>
             <WorkshopAbout />
+            <BenefitBiayaWorkshop/>
             <BottomRegistWork/>
         </>
     )

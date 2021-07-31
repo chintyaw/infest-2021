a5aed6334527b27905b37006a5cd4bc03c08c04d
Vincent541 <80274903+VincentChristian541@users.noreply.github.com>
2021-07-31 23:26:47 +0700

    Add files via upload
    Biaya dan benefit workshop + webinar

---
+++ src/components/BenefitBiayaWorkshop.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import './Compe.css'
+
+const BenefitBiayaWorkshop = () => {
+    return (
+        <div style={{marginBottom:"220px"}}>
+            <div style={{marginBottom:"220px"}}>
+                <h1 style={{textAlign: "center", marginBottom:"40px"}}>
+                Benefit
+                </h1>
+                <h2 style={{textAlign: "center", marginBottom:"20px"}}>
+                • Pengajaran secara langsung
+                </h2>
+                <h2 style={{textAlign: "center", marginBottom:"20px"}}>
+                • Hands on materi workshop
+                </h2>
+                <h2 style={{textAlign: "center", marginBottom:"20px"}}>
+                • Sertifikat untuk setiap peserta
+                </h2>
+            </div>
+            <h1 style={{textAlign: "center", marginBottom:"20px"}}>
+                Biaya
+            </h1>
+            <div className="Daftar">
+                Workshop-Rp 20.000,00
+                <br></br><br></br>
+                Workshop + Webinar Bundle-Rp. xx.xxx,xx
+            </div>
+        </div>
+    )
+}
+
+export default BenefitBiayaWorkshop

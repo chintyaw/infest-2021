ee0c876ff0a5ea6680c88697610326486c303c33
chintyaw <cwchintya@gmail.com>
2021-08-14 21:11:46 +0700

    add sponsor

--- src/components/Sponsor.js
+++ src/components/Sponsor.js
@@ -2,10 +2,13 @@ import React from 'react'
 
 const Sponsor = () => {
     return (
-        <div>
-            <h1 style={{textAlign: "center", marginBottom: "62px"}}>Sponsored By</h1>
-            <div style={{ width: "100%", display: "flex", justifyContent: "center"}}>
-                <img style={{ marginBottom: "200px", width: "50%", height: "auto", objectFit: "cover" }} src="/sponsor.svg" alt="/" />
+        <div style={{ marginBottom: "200px"}}>
+            <h1 style={{textAlign: "center", marginBottom: "0px"}}>Sponsored By</h1>
+            <div style={{ width: "100%", display: "flex", flexDirection: "column", justifyContent: "center"}}>
+                <a href="https://miraeasset.co.id/" target="_blank" rel="noreferrer" style={{ display: "flex", justifyContent: "center" }}>
+                    <img style={{ width: "60%", height: "auto", objectFit: "cover", margin: "auto", marginBottom: "50px"  }} src="/mirae-logo.png" alt="/" />
+                </a>
+                <img style={{ width: "70%", height: "auto", objectFit: "cover", margin: "auto" }} src="/sponsor.svg" alt="/" />
             </div>
         </div>
     )

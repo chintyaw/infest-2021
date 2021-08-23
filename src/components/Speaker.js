e2a2f44333d9374a33afbb101d8aa88cc2e842a9
graciellavl <graciellavl@users.noreply.github.com>
2021-08-23 21:31:22 +0700

    change icon

--- src/components/Speaker.js
+++ src/components/Speaker.js
@@ -9,11 +9,11 @@ function Speaker() {
           <h1>In-Depth Spoiler to Reach Financial Freedom</h1>
           <div className="datetime">
             <div className="date">
-              <i class="fa-solid fa-calendar-days"></i>
+              <i class="fa fa-calendar" aria-hidden="true"></i>
               <h3>25 September 2021</h3>
             </div>
             <div className="time">
-              <i class="fa-solid fa-clock-one"></i>
+              <i class="fa fa-clock" aria-hidden="true"></i>
               <h3>13.00 - 15.00 WIB</h3>
             </div>
           </div>
@@ -22,9 +22,7 @@ function Speaker() {
             <img src="/melvin-mumpuni.svg" alt="speaker" />
             <div className="text-speaker-right">
               <h2>Melvin Mumpuni</h2>
-              <h3>
-                • Founder of Finansialku
-              </h3>
+              <h3>• Founder of Finansialku</h3>
             </div>
           </div>
         </div>
@@ -33,11 +31,11 @@ function Speaker() {
           <h1>Unleashing Indonesia’s Post Pandemic Economic Potential</h1>
           <div className="datetime-right">
             <div className="date-right">
-              <i class="fa-solid fa-calendar-days"></i>
+              <i class="fa fa-calendar" aria-hidden="true"></i>
               <h3>25 September 2021</h3>
             </div>
             <div className="time-right">
-              <i class="fa-solid fa-clock-one"></i>
+              <i class="fa fa-clock" aria-hidden="true"></i>
               <h3>15.00 - 17.30 WIB</h3>
             </div>
           </div>
@@ -45,9 +43,7 @@ function Speaker() {
           <div className="speaker-right">
             <div className="text-speaker-left">
               <h2>Ilham Muslim</h2>
-              <h3>
-              • Mirae Asset
-              </h3>
+              <h3>• Mirae Asset</h3>
             </div>
             <img src="/ilham-muslim.svg" alt="speaker" />
           </div>
@@ -57,11 +53,11 @@ function Speaker() {
           <h1>Tech-Stocks, Take It Or Leave It</h1>
           <div className="datetime">
             <div className="date">
-              <i class="fa-solid fa-calendar-days"></i>
+              <i class="fa fa-calendar" aria-hidden="true"></i>
               <h3>26 September 2021</h3>
             </div>
             <div className="time">
-              <i class="fa-solid fa-clock-one"></i>
+              <i class="fa fa-clock" aria-hidden="true"></i>
               <h3>13.00 - 15.00 WIB</h3>
             </div>
           </div>
@@ -71,21 +67,23 @@ function Speaker() {
             <div className="text-speaker-right">
               <h2>Timothy Ronald</h2>
               <h3>
-                • Young Investor <br/> • CO-Founder of Ternak Uang
+                • Young Investor <br /> • CO-Founder of Ternak Uang
               </h3>
             </div>
           </div>
         </div>
 
         <div className="judul-speaker-right">
-          <h1>Maximizing Value and Growth Investing through Portfolio Management</h1>
+          <h1>
+            Maximizing Value and Growth Investing through Portfolio Management
+          </h1>
           <div className="datetime-right">
             <div className="date-right">
-              <i class="fa-solid fa-calendar-days"></i>
+              <i class="fa fa-calendar" aria-hidden="true"></i>
               <h3>26 September 2021</h3>
             </div>
             <div className="time-right">
-              <i class="fa-solid fa-clock-one"></i>
+              <i class="fa fa-clock" aria-hidden="true"></i>
               <h3>15.00 - 17.30 WIB</h3>
             </div>
           </div>
@@ -94,13 +92,13 @@ function Speaker() {
             <div className="text-speaker-left">
               <h2>Bernardus Wijaya</h2>
               <h3>
-              • CEO of Sucor Sekuritas<br/> • Forbes 30 Under 30 2021
+                • CEO of Sucor Sekuritas
+                <br /> • Forbes 30 Under 30 2021
               </h3>
             </div>
             <img src="/bernard-wijaya.svg" alt="speaker" />
           </div>
         </div>
-
       </div>
     </>
   );

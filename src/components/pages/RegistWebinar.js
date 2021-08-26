8b264013b58c1d4bf4d1992efa29a994ce533dc3
chintyaw <cwchintya@gmail.com>
2021-08-27 00:16:34 +0700

    add speaker

--- src/components/pages/RegistWebinar.js
+++ src/components/pages/RegistWebinar.js
@@ -3,7 +3,7 @@ import '../../App.css'
 import { Widget } from '@typeform/embed-react'
 
 const RegistWebinar = () => {
-    return <Widget id="YDMVdcb8" style= {{width: '100%', height: '580px', color: '#F1F4F8', paddingTop: "80px"}} className="my-form" />
+    return <Widget id="YDMVdcb8" style= {{width: '100%', height: 'calc(100vh - 80px)', color: '#F1F4F8', paddingTop: "80px"}} className="my-form" />
 }
 
 export default RegistWebinar;
